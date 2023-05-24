7d550dad12eb3b72c4ad7d31f92bed7c02e513d2
Javi G <javig@Javis-MacBook-Pro.local>
2023-05-24 11:03:42 +0200

    feat: recursive fibonacci

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,4 +1,8 @@
 export function fibonacci(n:number): number {
-  if (n > 0) return 1  
-  return 0
+  // if (n > 1) return n + 1
+
+
+  if (n == 0) return 0
+  if (n <= 2) return 1
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -1,5 +1,8 @@
 import { fibonacci } from '../core/fibonacci'
 
+
+// 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597 
+
 describe('fibonacci', () => {
 	it('first time returns 0', () => {
 		expect(fibonacci(0)).toBe(0)
@@ -7,8 +10,12 @@ describe('fibonacci', () => {
 	it('second time returns 1', () => {
 		expect(fibonacci(1)).toBe(1)
 	})
-	it('third time returns 1', () => {
-		expect(fibonacci(2)).toBe(1)
+	it('each time is the sum of the preceding two numbers', () => {
+		expect(fibonacci(2)).toBe(fibonacci(0) + fibonacci(1))
+		expect(fibonacci(3)).toBe(fibonacci(2) + fibonacci(1))
+		expect(fibonacci(4)).toBe(fibonacci(3) + fibonacci(2))
+		expect(fibonacci(5)).toBe(fibonacci(4) + fibonacci(3))
+		expect(fibonacci(8)).toBe(21)
 	})
 })
 
