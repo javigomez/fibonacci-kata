fb49f56f0bb010fed137f1d2fb3daaf24c961a3a
Javi G <javig@Javis-MacBook-Pro.local>
2023-05-24 10:14:38 +0200

    test: second time returns 1

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -4,5 +4,8 @@ describe('fibonacci', () => {
 	it('first time returns 0', () => {
 		expect(fibonacci(0)).toBe(0)
 	})
+	it('second time returns 1', () => {
+		expect(fibonacci(1)).toBe(1)
+	})
 })
 
